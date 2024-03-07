f824fa1dc16eed3c9a10e40ebe80df1b8c40de2c
KStar-coder <sharmakaustav04@gmail.com>
2024-03-07 23:16:46 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-xl text-center text-orange-600 bg-yellow-200'>Currency App</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({}) // putting an empty object because if fetch call does not happen, the program will loop through the empty object and it will not crash
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+            .then((res) => res.json())
+            .then((res) => setData(res[currency]))
+        console.log(data)
+    }, [currency])
+    console.log(data)
+    return data
+}
+
+export default useCurrencyInfo;
